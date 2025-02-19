2db607289de9b526a98484b516ca7efc2025e74b
User Name <ehrosales5@Laptop-de-Edwin.local>
2025-02-19 13:55:50 -0700

    final commit

---
+++ src/components/BurgerStack/BurgerStack.jsx
@@ -0,0 +1,24 @@
+// src/components/BurgerStack/BurgerStack.jsx
+
+import React from 'react'
+
+
+
+const BurgerStack = ({ stack, removeFromBurger }) => {
+    return (
+        <ul>
+            {stack.map((ingredient, idx) => (
+                <li key={idx}>
+                    <div>
+                        <p>{ingredient.name}</p>
+                        <button onClick={() => removeFromBurger(ingredient)}>Remove
+                        </button>
+                    </div>
+                </li>
+            ))}
+
+        </ul>
+    )
+}
+
+export default BurgerStack
